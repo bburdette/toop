9ac9af4866c71af4c37cccef9c38c8faf243c62b
Ben Burdette <bburdette@gmail.com>
2019-04-22 19:04:17 -0600

    docs stuff

--- src/Toop.elm
+++ src/Toop.elm
@@ -1,72 +1,91 @@
-module Toop exposing
-    ( T1(..)
-    , T10(..)
-    , T11(..)
-    , T12(..)
-    , T13(..)
-    , T14(..)
-    , T2(..)
-    , T3(..)
-    , T4(..)
-    , T5(..)
-    , T6(..)
-    , T7(..)
-    , T8(..)
-    , T9(..)
-    )
+module Toop exposing (T1(..), T2(..), T3(..), T4(..), T5(..), T6(..), T7(..), T8(..), T9(..), T10(..), T11(..), T12(..), T13(..), T14(..))
+
+{-| A set of tuple-like data structures, allowing more than 3 elements.
+
+@docs T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14
+
+-}
 
 
+{-| 1 element toop.
+-}
 type T1 a
     = T1 a
 
 
+{-| 2 element toop.
+-}
 type T2 a b
     = T2 a b
 
 
+{-| 3 element toop.
+-}
 type T3 a b c
     = T3 a b c
 
 
+{-| 4 element toop.
+-}
 type T4 a b c d
     = T4 a b c d
 
 
+{-| 5 element toop.
+-}
 type T5 a b c d e
     = T5 a b c d e
 
 
+{-| 6 element toop.
+-}
 type T6 a b c d e f
     = T6 a b c d e f
 
 
+{-| 7 element toop.
+-}
 type T7 a b c d e f g
     = T7 a b c d e f g
 
 
+{-| 8 element toop.
+-}
 type T8 a b c d e f g h
     = T8 a b c d e f g h
 
 
+{-| 9 element toop.
+-}
 type T9 a b c d e f g h i
     = T9 a b c d e f g h i
 
 
+{-| 10 element toop.
+-}
 type T10 a b c d e f g h i j
     = T10 a b c d e f g h i j
 
 
+{-| 11 element toop.
+-}
 type T11 a b c d e f g h i j k
     = T11 a b c d e f g h i j k
 
 
+{-| 12 element toop.
+-}
 type T12 a b c d e f g h i j k l
     = T12 a b c d e f g h i j k l
 
 
+{-| 13 element toop.
+-}
 type T13 a b c d e f g h i j k l m
     = T13 a b c d e f g h i j k l m
 
 
+{-| 14 element toop.
+-}
 type T14 a b c d e f g h i j k l m n
     = T14 a b c d e f g h i j k l m n
--- src/Toop/Take.elm
+++ src/Toop/Take.elm
@@ -1,33 +1,31 @@
-module Toop.Take exposing
-    ( takeT1
-    , takeT10
-    , takeT11
-    , takeT12
-    , takeT13
-    , takeT14
-    , takeT2
-    , takeT3
-    , takeT4
-    , takeT5
-    , takeT6
-    , takeT7
-    , takeT8
-    , takeT9
-    , tuply
-    )
+module Toop.Take exposing (takeT1, takeT2, takeT3, takeT4, takeT5, takeT6, takeT7, takeT8, takeT9, takeT10, takeT11, takeT12, takeT13, takeT14)
+
+{-| functions to make an N element toop from a List.
+
+@docs takeT1, takeT2, takeT3, takeT4, takeT5, takeT6, takeT7, takeT8, takeT9, takeT10, takeT11, takeT12, takeT13, takeT14
+
+-}
+
+import Toop exposing (..)
 
 
+{-| 1 element toop.
+-}
 takeT1 : List a -> Maybe ( T1 a, List a )
 takeT1 la =
     andX T1 la
 
 
+{-| 2 element toop.
+-}
 takeT2 : List a -> Maybe ( T2 a a, List a )
 takeT2 l =
     andX T2 l
         |> Maybe.andThen (tuply andX)
 
 
+{-| 3 element take.
+-}
 takeT3 : List a -> Maybe ( T3 a a a, List a )
 takeT3 l =
     andX T3 l
@@ -35,6 +33,8 @@ takeT3 l =
         |> Maybe.andThen (tuply andX)
 
 
+{-| 4 element take.
+-}
 takeT4 : List a -> Maybe ( T4 a a a a, List a )
 takeT4 l =
     andX T4 l
@@ -43,6 +43,8 @@ takeT4 l =
         |> Maybe.andThen (tuply andX)
 
 
+{-| 5 element take.
+-}
 takeT5 : List a -> Maybe ( T5 a a a a a, List a )
 takeT5 l =
     andX T5 l
@@ -52,6 +54,8 @@ takeT5 l =
         |> Maybe.andThen (tuply andX)
 
 
+{-| 6 element take.
+-}
 takeT6 : List a -> Maybe ( T6 a a a a a a, List a )
 takeT6 l =
     andX T6 l
@@ -62,6 +66,8 @@ takeT6 l =
         |> Maybe.andThen (tuply andX)
 
 
+{-| 7 element take.
+-}
 takeT7 : List a -> Maybe ( T7 a a a a a a a, List a )
 takeT7 l =
     andX T7 l
@@ -73,6 +79,8 @@ takeT7 l =
         |> Maybe.andThen (tuply andX)
 
 
+{-| 8 element take.
+-}
 takeT8 : List a -> Maybe ( T8 a a a a a a a a, List a )
 takeT8 l =
     andX T8 l
@@ -85,6 +93,8 @@ takeT8 l =
         |> Maybe.andThen (tuply andX)
 
 
+{-| 9 element take.
+-}
 takeT9 : List a -> Maybe ( T9 a a a a a a a a a, List a )
 takeT9 l =
     andX T9 l
@@ -98,6 +108,8 @@ takeT9 l =
         |> Maybe.andThen (tuply andX)
 
 
+{-| 10 element take.
+-}
 takeT10 : List a -> Maybe ( T10 a a a a a a a a a a, List a )
 takeT10 l =
     andX T10 l
@@ -112,6 +124,8 @@ takeT10 l =
         |> Maybe.andThen (tuply andX)
 
 
+{-| 11 element take.
+-}
 takeT11 : List a -> Maybe ( T11 a a a a a a a a a a a, List a )
 takeT11 l =
     andX T11 l
@@ -127,6 +141,8 @@ takeT11 l =
         |> Maybe.andThen (tuply andX)
 
 
+{-| 12 element take.
+-}
 takeT12 : List a -> Maybe ( T12 a a a a a a a a a a a a, List a )
 takeT12 l =
     andX T12 l
@@ -143,6 +159,8 @@ takeT12 l =
         |> Maybe.andThen (tuply andX)
 
 
+{-| 13 element take.
+-}
 takeT13 : List a -> Maybe ( T13 a a a a a a a a a a a a a, List a )
 takeT13 l =
     andX T13 l
@@ -160,6 +178,8 @@ takeT13 l =
         |> Maybe.andThen (tuply andX)
 
 
+{-| 14 element take.
+-}
 takeT14 : List a -> Maybe ( T14 a a a a a a a a a a a a a a, List a )
 takeT14 l =
     andX T14 l
